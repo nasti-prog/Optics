0f1953653e6ebeff19f46b3f55f180115fc3cb3e
Nastya Lip <nastlip2006@gmail.com>
2026-04-23 23:51:31 +0400

    Add files via upload

--- main.m
+++ main.m
@@ -2,28 +2,16 @@
 
 p0 = [0, 0, 1];
 
-x = [1, 2];
-y = [2, 3];
+% Parameter of output plane
+Z = 1000;
+
+x = 0:0.5:5;        % равномерное распределение
+y = 0:0.5:5;
 [X, Y] = meshgrid(x, y);
 
-u = [3, 5];
-v = [4, 6];
+u = rand(1, 10);
+v = rand(1, 10);
 [U, V] = meshgrid(u, v);
-
-% Parameter h_0
-h_0 = rand(4, 1);
-
-% Normals & orths
-Normals = rand(4, 3);
-
-N1 = get_orth(Normals(1, :));
-disp (N1);
-N2 = get_orth(Normals(2, :));
-N3 = get_orth(Normals(3, :));
-N4 = get_orth(Normals(4, :));
-
-% Parameter of output plane
-Z = 10;
 
 % p1
 p1_1 = [U(1,1), V(1,1), Z];
@@ -31,11 +19,39 @@ p1_2 = [U(2,1), V(2,1), Z];
 p1_3 = [U(1,2), V(1,2), Z];
 p1_4 = [U(2,2), V(2,2), Z];
 
+% Normals & orths
+Normals = rand(50, 3);
+N1 = get_orth(Normals(1, :));
+
 % Angles for 1st plane
 inc_ang1 = angle(N1, p0);
 ref_ang1 = angle(N1, p1_1);
 
-% To create Plane should be used h_0 and Normal  with the same index
+% Parameter h_0
+h_0 = zeros(50, 1);
+h_0(:, 1) = 10;
+
+% Min distance & index
+Min = zeros(11, 11);
+Index_plane = zeros(11, 11);
+
+for i = 1:11                      % Count of beams
+    for j = 1:11
+        x = X(i, j);
+        y = Y(i, j);
+        min_dist = 1000000;
+        index_plane = 1;
+        for plane = 1:50
+            z = distance(plane, x, y, h_0(plane), Normals);
+            if z < min_dist
+                min_dist = z;
+                index_plane = plane;
+            end
+        end
+        Min(i, j) = min_dist;
+        Index_plane(i, j) = index_plane;
+    end
+end
 %% Functions
 
 % Find angle in degrees between vec1 & vec2
@@ -56,4 +72,28 @@ end
 % Refracted beam - p1
 function [Normal] = get_normal (n1, n2, inc_vec, ref_vec, inc_ang, ref_ang )
     Normal = ( (n1*inc_vec - n2*ref_vec) / (n1*cosd(inc_ang) - n2*cosd(ref_ang)) );
+end
+
+% Distance to plane
+% Ni - components of Normal-vector
+% (x, y) - start coord of inc_beam
+% h_0 - parameter of the plane
+function [z] = distance(index, x, y, h_0, Normals)
+    Normal_orth = get_orth(Normals(index, :));
+    N1 = Normal_orth(1, 1); 
+    N2 = Normal_orth(1, 2);
+    N3 = Normal_orth(1, 3);
+    len = sqrt(dot(Normal_orth, Normal_orth));
+    disp(len);
+    z = (N3*h_0 - N1*x - N2*y)/N3;
+end
+
+% Evklid distance to plane
+function [d] = evkild_distance(index, x_0, y_0, h_0, Normals)
+    Normal_orth = get_orth(Normals(index, :));
+    N1 = Normal_orth(1, 1);
+    N2 = Normal_orth(1, 2);
+    N3 = Normal_orth(1, 3);
+    len_N = sqrt( dot(Normals(index, :), Normals(index, :)) );
+    d = abs(N1*x_0 + N2*y_0 + N3*h_0)/len_N;
 end
