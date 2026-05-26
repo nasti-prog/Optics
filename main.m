9811abb07145f148d34d03ff3d5f459c54effe3e
Nastilip <nastlip2006@gmail.com>
2026-05-26 23:06:07 +0400

    Beams

--- main.m
+++ main.m
@@ -1,45 +1,40 @@
 %% Distribution of Energy
 
-%Input data
+%Input data.
 p0 = [0, 0, 1];
-Z = 1000;                                         % Parameter of output plane
+Z = 1000;                                                   % Parameter of output plane
 
 n1 = 1.48;
 n2 = 1;
 
-x = 0:0.5:2.5;                                      % равномерное распределение
+x = 0:0.5:2.5;                                              % равномерное распределение
 y = 0:0.5:2.5;
 [X, Y] = meshgrid(x, y);
-inc_beams = length(x)*length(y)                 % count of incident beams
+inc_beams = length(x)*length(y);                            %count of incident beams
+Aperture = [X(:), Y(:)];                                    % pair of coords by rows
 
 u = 0:1:7;
 v = 0:1:7;
 [U, V] = meshgrid(u, v);
-refr_beams = length(u)*length(v)                % count of refracted beams
+refr_beams = length(u)*length(v);                            % count of refracted beams
+Display = [U(:), V(:)];
 
-h_0 = 9 + (10-9)* rand(50, 1);                 % Parameter h_0 - count of planes
-%Planes = size(h_0, 1);
+h_0 = 9 + (10-9)* rand(refr_beams, 1);                       % Parameter h_0 - count of planes
 
-Energy_inc = 2*ones(1, inc_beams);                  % Energy
-Energy_refr = zeros(1, refr_beams);
+Iter = 1000;                                                 % Count of iterations
+Ismin = false;                                               % alg requires min
+alpha = 1;                                                   % trained num;
+Energy_inc = 2*ones(1, inc_beams);                           % Energy
+Energy_refr = 2*ones(1, refr_beams);
+Energy_req = 6*ones(1, refr_beams);
 
-p1 = zeros(refr_beams, 3);                      % p1
-for i = 0 : length(u)-1
-    for j = 0 : length(v)-1
-        p1(i * length(v) + j + 1, :) = [U(i+1, j+1), V(i+1, j+1), Z];
-    end
-end
+j = 1:refr_beams;
+p1 = zeros(refr_beams, 3);                                  % p1         
+p1(j, :) = [Display(j, 1), Display(j, 2), repmat(Z, refr_beams, 1)];
 
-% Normals & orths
-Normals = zeros(50, 3);
-for i = 1:50              
-    Normals(i, :) = get_normal (n1, n2, p0, p1(i, :));
-end
-
-%Check plane 1
-ff = get_normal(n1, n2, p0, [12, 0, Z]);
-distance_z(0, 0, ff, h_0(1, 1));
-distance_z(4, 0, ff, h_0(1, 1));
+Normals = zeros(refr_beams, 3);                             % Normals & orths
+A = repmat(p0, refr_beams, 1);
+Normals(j, :) = get_normal(n1, n2, A(j, :), p1(j, :));
 
 %Check plane 2
 ff = get_normal(n1, n2, p0, [6, 0, Z]);
@@ -49,75 +44,50 @@ distance_z(0.5, 0, ff, h_0(2, 1));
 % View of plane
 %plane1 = visual_plane(Normals(2, :), h_0(2, 1));
 
-% Distance & min, max dist to planes for each incident beam
-% Array_beam of dist to planes
-% Column i – beam i, row i – plane i
+Dist_beam = zeros (refr_beams, inc_beams);                                                      % row i – plane i, column i – beam i
+count = 0;
 
-Dist_beam = zeros (50, inc_beams);
-for cord_x = 0 : length(x)-1
-    for cord_y = 0 : length(y)-1
-        for plane = 1:50
-            z = distance_z(cord_x, cord_y, Normals(plane, :), h_0(plane, :));
-            Dist_beam(plane, cord_x*length(y) + cord_y + 1) = z;
+for count = 1:2      
+
+    for i = 1:inc_beams
+        for j = 1:refr_beams
+            Dist_beam(j, i) = distance_Z(Aperture(i, :), Normals(j, :), h_0(j));         % dist to planes for each inc beam
         end
-    end 
-end
-
-Dist_min = zeros(1, inc_beams);
-
-Dist_min = min(Dist_beam);
-
-
-Dist_max = zeros(1, inc_beams);
-for n = 1:inc_beams
-    [Dist_max(2, n), Dist_max(1, n)] = max (Dist_beam(:, n));
-end
-
-
-Dist_maxi = zeros(1, inc_beams);
-[Dist_maxi(2, :), Dist_maxi(1, :)] = max (Dist_beam);
-
-
-
-
-for i = 1:inc_beams
-    Plane = Dist_max(1, i);
-    Energy_refr(1, Plane) = Energy_refr(1, Plane) + Energy_inc(1, i);
-end
-
-Dist_beams = zeros (50, 1);
-Coords = [X(:), Y(:)];      % pair of coords by rows
-for i = 1:inc_beams
-    for plane = 1:50
-        z = distance_Z(Coords(i, :), Normals(plane, :), h_0(plane, :));
-        Dist_beams(plane, i) = z;
     end
+
+    Dist_need = zeros(1, inc_beams);
+    if Ismin 
+        [Dist_need(2, :), Dist_need(1, :)] = min (Dist_beam);
+    else 
+        [Dist_need(2, :), Dist_need(1, :)] = max (Dist_beam);
+    end
+
+    Plane = Dist_need(1, i);
+    Energy_refr(1, Plane) = Energy_refr(1, Plane) + Energy_inc(1, i);
+
+    h_0 = h_0 + alpha * (Energy_req - Energy_refr);
 end
-%Zak = (Normals(1, 3)*h_0(1, 1) - Normals(1, 1)*X - Normals(1, 2)*Y)/Normals(1, 3)
-%Dist_mini = zeros(1, inc_beams);
-%for planes = 1:50
-%    %Dist_beam = distance_Z(Matrix);    %matrix function
-%    Dist_mini(1, plane) = min(Matrix);
-%end
+deltaE = Energy_req - Energy_refr;
+count;
 %% Functions
 
 % Create orth from a vector
-function [orth] = get_orth (vector)
-    len = sqrt(dot(vector, vector));
-    orth = vector/len;
+function [orth] = get_orth (matrix)
+orth = zeros(size(matrix, 1), size(matrix, 2));
+    for e = 1 : size(matrix, 1)
+        len = sqrt(dot(matrix(e, :), matrix(e, :)));
+        orth(e, :) = matrix(e, :)/len;
+    end
 end
 
-% Create Normal - orth(from 1 to 2 env) with n1, n2, incident_vec, refracted_vec, incident_ang, refracted_ang 
-% Incident beam - p0 - eд вектор
-% Refracted beam - p1
+% Create Normal - orth(from 1 to 2 env) with n1, n2, incident_vec, refracted_vec, incident_ang, refracted_ang
 function [Normal] = get_normal (n1, n2, inc_vec, ref_vec)
     inc = get_orth(inc_vec);
     refr = get_orth(ref_vec);
-    Normal = ( (n1*inc - n2*refr) / sqrt(dot(n1*inc - n2*refr, n1*inc - n2*refr)) );
+    Normal = ( (n1.*inc - n2.*refr) ./ sqrt(dot(n1.*inc - n2.*refr, n1.*inc - n2.*refr, 2)) );
 end
 
 % Distance to plane
-% Ni - components of Normal-vector
 % (x, y) - start coord of inc_beam
 % h_0 - parameter of the plane
 function [z] = distance_z(x, y, Normal, h0)
@@ -127,7 +97,7 @@ end
 
 function [z] = distance_Z(Coords, Normal, h0)
     Normal_orth = get_orth(Normal);
-    z = (Normal_orth(1, 3)*h0 - Normal_orth(1, 1)*Coords(1, 1) - Normal_orth(1, 2)*Coords(1, 2))/Normal_orth(1, 3);
+    z = (Normal_orth(1, 3).*h0 - Normal_orth(1, 1).* Coords(1, 1) - Normal_orth(1, 2).*Coords(1, 2))./Normal_orth(1, 3);
 end
 
 % Plane visualising
