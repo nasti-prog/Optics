00ca61b38f42a32868d9a73b2168f2f018b8b96d
Nastilip <nastlip2006@gmail.com>
2026-07-16 14:07:13 +0400

    last version

--- RRMSE.m
+++ RRMSE.m
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 function [rrmse] = RRMSE(matr_req, matr_refr)
 rrmse = ( rmse(matr_req, matr_refr) / (sum(matr_req)/length(matr_req)) )*100;
 end
-
-=======
-function [rrmse] = RRMSE(matr_req, matr_refr)
-rrmse = ( rmse(matr_req, matr_refr) / (sum(matr_req)/length(matr_req)) )*100;
-end
-
->>>>>>> 47cd48257b104dc7b5e901879d38e70df5f4d28d
